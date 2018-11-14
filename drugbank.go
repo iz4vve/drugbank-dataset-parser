3193a0727d3b5765cf8cdaa90cd5955ed1f6ff62
Pietro Mascolo <iz4vve@gmail.com>
2018-11-14 15:58:27 +0000

    ADDS socumentation

--- drugbank.go
+++ drugbank.go
@@ -1,23 +1,37 @@
 package main
 
+// TODO
+//
+// <transporters/> -> complex structure containing a polypeptide.
+// 					  may it's not worth the hassle given the info
+// <transmembrane-regions/> -> it's within a Polypeptide
+
 import (
+	"bufio"
 	"encoding/xml"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/schollz/progressbar"
 )
 
 func main() {
+	defer TimeTrack("main", time.Now())
 	xmlFile, err := os.Open("drugbank.xml")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer xmlFile.Close()
 	decoder := xml.NewDecoder(xmlFile)
-
+	numberOfDrugs := getDrugsNumber(xmlFile)
+	bar := progressbar.New(numberOfDrugs)
 	drugs := []Drug{}
-
+	xmlFile.Seek(0, 0)
 	count := 0
-
 	for {
 		token, _ := decoder.Token()
 		if token == nil {
@@ -32,24 +46,50 @@ func main() {
 			}
 		}
 		count++
+		bar.Add(1)
 		if count%500 == 0 {
 			break
 		}
 	}
-	// for _, d := range drugs {
-	// 	if len(d.Carriers) > 0 {
-	// 		fmt.Println("---------------------------")
-	// 		s, _ := json.MarshalIndent(d.Carriers, "", "\t")
-	// 		fmt.Println(string(s))
-	// 	}
-	// }
 }
 
+// getDrugsNumber counts the number of opening drug tags
+// in the xml file. Th tags that contain attributes are top-level
+// tags for drugs
+func getDrugsNumber(file io.Reader) int {
+	defer TimeTrack("getDrugsNumber", time.Now())
+	counter := 0
+	pattern := "<drug type="
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), pattern) {
+			counter++
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return counter
+}
+
+// TimeTrack tracks the execution time of a function
+func TimeTrack(name string, start time.Time) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %.2f seconds\n", name, elapsed.Seconds())
+}
+
+// Drug represents a drug and all its related information
+// More detailed info available at https://www.drugbank.ca/documentation#drug-cards
 type Drug struct {
 	ID                     string               `xml:"drugbank-id"`
+	DrugRecordCreatedOn    string               `xml:"created,attr"`
+	DrugRecordUpdatedOn    string               `xml:"updated,attr"`
+	DrugType               string               `xml:"type,attr"`
 	Name                   string               `xml:"name"`
 	Description            string               `xml:"description"`
-	CAS                    string               `xml:"cas-number"`
+	CAS                    string               `xml:"cas-number"` // Chemical Abstract Service identification number
 	UNII                   string               `xml:"unii"`
 	State                  string               `xml:"state"`
 	Groups                 []Group              `xml:"groups"`
@@ -74,7 +114,7 @@ type Drug struct {
 	Categories             []Category           `xml:"categories"`
 	AffectedOrganisms      []Organism           `xml:"affected-organisms"`
 	Dosages                []Dosage             `xml:"dosages"`
-	ATCCodes               []ATCCode            `xml:"atc-codes"`
+	ATCCodes               []ATCCode            `xml:"atc-codes"` // WHO drug classification system (ATC) identifiers
 	FDALabel               string               `xml:"fda-label"`
 	MSDS                   string               `xml:"msds"`
 	Patents                []Patent             `xml:"patents"`
@@ -83,8 +123,8 @@ type Drug struct {
 	ExperimentalProperties []Property           `xml:"experimental-properties>property"`
 	ExternalIdentifiers    []ExternalIdentifier `xml:"external-identifiers>external-identifier"`
 	ExternalLinks          []ExternalLink       `xml:"external-links>external-link"`
-	Pathways               []Pathway            `xml:"pathways>pathway"`
 	Targets                []Target             `xml:"targets>target"`
+	Pathways               []Pathway            `xml:"pathways>pathway"`
 	SynthesysReference     string               `xml:"synthesis-reference"`
 	ProteinBinding         string               `xml:"protein-binding"`
 	Salts                  []Salt               `xml:"salts>salt"`
@@ -98,6 +138,14 @@ type Drug struct {
 	Carriers               []Carrier            `xml:"carriers>carrier"`
 }
 
+// Carrier represents a secreted protein which binds to drugs,
+// carrying them to cell transporters, where they are moved into the cell.
+// Drug carriers may be used in drug design to increase the
+// effectiveness of drug delivery to the target sites of pharmacological actions.
+// Targets, Enzymes, Carriers, And Transporters may switch roles depending
+// on the drug to which they bind. Some drugs specifically target transporters,
+// and in this case a transporter can also be the target
+// (for example: Procaine targeting the Sodium-dependent dopamine transporter).
 type Carrier struct {
 	Position    string      `xml:"position,attr"`
 	ID          string      `xml:"id"`
@@ -109,6 +157,7 @@ type Carrier struct {
 	Polypeptide Polypeptide `xml:"polypeptide"`
 }
 
+// AdverseReaction represents a possible adverse reaction a drug may cause
 type AdverseReaction struct {
 	ProteinName     string `xml:"protein-name"`
 	GeneSymbol      string `xml:"gene-symbol"`
@@ -119,6 +168,9 @@ type AdverseReaction struct {
 	PubmedID        string `xml:"pubmed-id"`
 }
 
+// SNPEffect identifies possible nucleotide mutations
+// a drug might cause.
+// SNP -> Single Nucleotide Polymorphism
 type SNPEffect struct {
 	ProteinName    string `xml:"protein-name"`
 	GeneSymbol     string `xml:"gene-symbol"`
@@ -130,6 +182,8 @@ type SNPEffect struct {
 	PubmedID       string `xml:"pubmed-id"`
 }
 
+// Reaction describes a reaction a specific drug can undergo with
+// another reagent
 type Reaction struct {
 	Sequence string `xml:"sequence"`
 	Left     struct {
@@ -143,6 +197,7 @@ type Reaction struct {
 	Enzymes []string `xml:"uniprot-id"`
 }
 
+// Salt represents a salt in which a drug can present itself
 type Salt struct {
 	ID        string `xml:"drugbank-id"`
 	Name      string `xml:"name"`
@@ -151,16 +206,18 @@ type Salt struct {
 	InchiKey  string `xml:"inchikey"`
 }
 
+// Brand identifies brands for mixtures or brand names
 type Brand struct {
 	Name    string `xml:"name"`
 	Company string `xml:"company"`
 }
 
-// <carriers/>
-// <transporters/>
-
-//     <transmembrane-regions/>   Polypeptide
-
+// Target represents a protein, macromolecule, nucleic acid,
+// or small molecule to which a given drug binds,
+// resulting in an alteration of the normal function of the
+// bound molecule and a desirable therapeutic effect.
+// Drug targets are most commonly proteins such as enzymes,
+// ion channels, and receptors.
 type Target struct {
 	ID          string      `xml:"id"`
 	Organism    string      `xml:"organism"`
@@ -170,6 +227,7 @@ type Target struct {
 	Polypeptide Polypeptide `xml:"polypeptide"`
 }
 
+// Polypeptide represents a single polypeptide and its relative details
 type Polypeptide struct {
 	ID                 string `xml:"id,attr"`
 	Source             string `xml:"source,attr"`
@@ -201,16 +259,25 @@ type Polypeptide struct {
 	GoClassifiers []GoClassifier `xml:"go-classifiers>go-classifier"`
 }
 
+// Pfam represents names and ID numbers of PFAM domains
 type Pfam struct {
 	Identifier string `xml:"identifier"`
 	Name       string `xml:"name"`
 }
 
+// GoClassifier represents Gene ontology classification
+// including function, cellular process and location
 type GoClassifier struct {
 	Category    string `xml:"category"`
 	Description string `xml:"description"`
 }
 
+// Pathway represents  processes (from SMPD) that the given molecule is involved in
+// A protein, macromolecule, nucleic acid, or small molecule
+// to which a given drug binds, resulting in an alteration of the
+// normal function of the bound molecule and a desirable therapeutic effect.
+// Drug targets are most commonly proteins such as enzymes,
+// ion channels, and receptors.
 type Pathway struct {
 	SMPDBID  string        `xml:"smpdb-id"`
 	Name     string        `xml:"name"`
@@ -219,41 +286,52 @@ type Pathway struct {
 	Enzymes  []Enzyme      `xml:"enzymes"`
 }
 
+// PathwayDrug identifies drugs involved with pathways
 type PathwayDrug struct {
 	ID   string `xml:"drugbank-id"`
 	Name string `xml:"name"`
 }
 
+// Enzyme contains the enzyme ID on UNIPROT
 type Enzyme struct {
 	UNIPROTID string `xml:"uniprot-id"`
 }
 
+// ExternalLink is a link to an external resource
 type ExternalLink struct {
 	Resource string `xml:"resource"`
 	URL      string `xml:"url"`
 }
 
+// ExternalIdentifier is an identifier to
+// link a drug to external resources
 type ExternalIdentifier struct {
 	Resource   string `xml:"resource"`
 	Identifier string `xml:"identifier"`
 }
 
+// Property represents a property of a drug as recorded in the source
 type Property struct {
 	Kind   string `xml:"kind"`
 	Value  string `xml:"value"`
 	Source string `xml:"source"`
 }
+
+// Sequence represents a sequence of aminoacids
+// and the format in which it is represented
 type Sequence struct {
 	Format   string `xml:"format,attr"`
 	Sequence string `xml:",chardata"`
 }
 
+// DrugInteraction represents a possible interaction between to drugs
 type DrugInteraction struct {
 	ID          string `xml:"drug-interaction>drugbank-id"`
 	Name        string `xml:"drug-interaction>name"`
 	Description string `xml:"drug-interaction>description"`
 }
 
+// Patent represents a Patent related to the drug
 type Patent struct {
 	Number    string `xml:"patent>number"`
 	Country   string `xml:"patent>country"`
@@ -262,37 +340,44 @@ type Patent struct {
 	Pediatric bool   `xml:"patent>pediatric-extension"`
 }
 
+// Dosage describes the dosage in which a drug is
+// to be administered and the route it should take.
 type Dosage struct {
 	Form     string `xml:"dosage>form"`
 	Route    string `xml:"dosage>route"`
 	Strength string `xml:"dosage>strength"` // TODO
 }
 
+// ATCCode represents the WHO drug classification system (ATC) identifiers
 type ATCCode struct {
 	Code struct {
 		Code   string `xml:"code,attr"`
 		Levels []struct {
 			Code        string `xml:"code,attr"`
 			Description string `xml:",chardata"`
-		} `xml:",any"` // TODO parse into a new struct (Level)
+		} `xml:",any"`
 	} `xml:"atc-code"`
 }
 
+// Mixture describes a mixture in which a drug can be found
 type Mixture struct {
 	Name        string `xml:"mixture>name"`
 	Ingredients string `xml:"mixture>ingredients"`
 }
 
+// Packager describes a packager of the drug
 type Packager struct {
 	Name string `xml:"packager>name"`
 	URL  string `xml:"packager>url"`
 }
 
+// Manufacturer describes the manufacturer of a mixture
 type Manufacturer struct {
 	Name string `xml:"manufacturer"`
 	URL  string `xml:"url,attr"`
 }
 
+// Price details the cost and currency of a medication
 type Price struct {
 	Description string `xml:"price>description"`
 	Details     struct {
@@ -302,21 +387,25 @@ type Price struct {
 	Unit string `xml:"price>unit"`
 }
 
+// Category represents a category of sub-division
 type Category struct {
 	Category string `xml:"category>category"`
 	MeshID   string `xml:"category>mesh-id"`
 }
 
+// Organism describes an organism affected by a drug
 type Organism struct {
 	Description string `xml:"affected-organism"`
 }
 
+// Synonym describes a synonym of a drug
 type Synonym struct {
 	Language string `xml:"language,attr"`
 	Coder    string `xml:"coder,attr"`
 	Synonym  string `xml:"synonym"`
 }
 
+// Classification describes the class of a substance
 type Classification struct {
 	Description string `xml:"description"`
 	Parent      string `xml:"direct-parent"`
@@ -326,6 +415,7 @@ type Classification struct {
 	Subclass    string `xml:"subclass"`
 }
 
+// Product represents a product in which a drug can be found.
 type Product struct {
 	Name                 string `xml:"product>name"`
 	Labeller             string `xml:"product>labeller"`
@@ -347,26 +437,31 @@ type Product struct {
 	Source               string `xml:"product>source"`
 }
 
+// Group describes a category
 type Group struct {
 	Name string `xml:"group"`
 }
 
+// Reference contains information on publications involving a drug
 type Reference struct {
 	Articles []Article `xml:"articles"`
 	Books    []Book    `xml:"textbooks"`
 	Links    []Link    `xml:"links"`
 }
 
+// Article represents a scientific paper regarding a drug
 type Article struct {
 	PubMedID string `xml:"article>pubmed-id"`
 	Citation string `xml:"article>citation"`
 }
 
+// Book represents a textbook regarding a drug
 type Book struct {
 	ISBN     string `xml:"textbook>isbn"`
 	Citation string `xml:"textbook>citation"`
 }
 
+// Link is th elink to a resource containing information regarding a drug
 type Link struct {
 	Title string `xml:"link>title"`
 	URL   string `xml:"link>url"`
